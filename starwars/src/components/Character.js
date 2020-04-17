4198a3680b800ce397782a3d290ce8bfb025ee44
Michael Gregory <michael.gregory8584@gmail.com>
2020-04-17 11:30:19 -0500

    Chose Rick and Morty because I was sure it was up and easier to use. got names popping up on screen 20 a ta time so far

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react';
+
+function Character(character){
+
+    return (
+
+        <div>
+            <h2>{character.name}</h2>
+
+        </div>
+
+    );
+
+}
+
+export default Character
